f46a964d1079e0f18b8df06be06a77ef742d0593
parzzzival8080 <bellojaezar>
2019-11-14 08:40:49 +0800

    g

--- trabahakita.com/resources/views/layouts/app.blade.php
+++ trabahakita.com/resources/views/layouts/app.blade.php
@@ -44,10 +44,6 @@
             
             @endif
            
-            <li class="nav-item">
-           
-            </li>
-            @endif
              
           <li class="nav-item dropdown">     
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
